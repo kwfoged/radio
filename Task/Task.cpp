925436b9a315ea7987ca2efd15c7e7bf8b2afbea
kwfoged <41474688+kwfoged@users.noreply.github.com>
2018-07-21 11:54:12 +0200

    Rename Task.cpp to Task/Task.cpp

---
+++ Task/Task.cpp
@@ -0,0 +1,19 @@
+#include "Task.h"
+
+Task::Task(unsigned short delta_time)
+{
+  mDeltaTime = delta_time;
+  mEntryTime = 0;
+}
+
+bool Task::StartTask(void)
+{
+  if(millis() > mEntryTime + mDeltaTime)
+  {
+   mEntryTime = millis();
+   return true;  
+  }
+  else
+    return false;   
+}
+  
