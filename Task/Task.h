4d7caeba5d8bfcea219b4a10a7d8932b8f4ef57d
kwfoged <41474688+kwfoged@users.noreply.github.com>
2018-07-21 11:54:30 +0200

    Rename Task.h to Task/Task.h

---
+++ Task/Task.h
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+class Task
+{  
+  public:
+    Task(unsigned short delta_time);
+    bool StartTask(void);
+  
+    unsigned long   mEntryTime;
+    unsigned short mDeltaTime;
+  
+
+  };
